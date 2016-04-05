00dfddb3c894c67f05dee9e64912dbea597dc448
Aashie <aashutoshbadal.1227@gmail.com>
2016-04-05 13:32:16 +0545

    file deleted

---
+++ add.php
@@ -0,0 +1,37 @@
+<?php
+//foreach
+$arr = $_POST;
+
+
+
+function td_td (){
+
+ "<td>" . ucfirst( $key) . "</td>";
+   <td>  
+
+   		if ( $key ==  "remember_me" ) {
+   			echo "Remember";
+   		} else {
+			echo $value; 			
+   		}
+
+
+}
+td_td()
+?>
+ 
+
+
+<table>
+<?php foreach ( $arr as $key => $value ){ ?>
+<tr>
+   
+   
+
+   ?></td>
+
+</tr>
+    <?php } ?>
+
+</table>
+
